18a7f537547ed20940474b156ca35a0f1260b2e2
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2023-03-26 12:42:53 +1100

    Added messages and a basic composer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,121 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    alignment::Vertical,
+    executor,
+    widget::{scrollable, Column, Container, Scrollable, Text, TextInput},
+    Application, Command, Length, Theme,
+};
+use once_cell::sync::Lazy;
+
+fn main() -> iced::Result {
+    Client::run(iced::Settings::default())
+}
+
+#[derive(Clone)]
+struct Message {
+    sender: String,
+    contents: String,
+}
+
+#[derive(Default)]
+struct Client {
+    username: String,
+    compose_value: String,
+    messages: Vec<Message>,
+}
+
+#[derive(Debug, Clone)]
+enum ClientMessage {
+    ComposerTyped(String),
+    MessageSubmitted,
+}
+
+static SCROLLABLE_ID: Lazy<scrollable::Id> = Lazy::new(scrollable::Id::unique);
+
+impl Application for Client {
+    type Executor = executor::Default;
+    type Message = ClientMessage;
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        (
+            Self {
+                username: "Brynblack".into(),
+                ..Default::default()
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        "ReoChat".into()
+    }
+
+    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+        match message {
+            ClientMessage::ComposerTyped(s) => {
+                self.compose_value = s;
+                Command::none()
+            }
+            ClientMessage::MessageSubmitted => match self.compose_value.as_str() {
+                "" => Command::none(),
+                _ => {
+                    let message = Message {
+                        sender: self.username.clone(),
+                        contents: self.compose_value.clone(),
+                    };
+
+                    self.messages.push(message);
+                    self.compose_value.clear();
+
+                    scrollable::snap_to(SCROLLABLE_ID.clone(), scrollable::RelativeOffset::END)
+                }
+            },
+        }
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
+        let messages = Container::new(
+            Scrollable::new(
+                Column::with_children(
+                    self.messages
+                        .clone()
+                        .into_iter()
+                        .map(|msg| {
+                            Column::new()
+                                .push(Text::new(msg.sender))
+                                .push(Text::new(msg.contents))
+                                .into()
+                        })
+                        .collect(),
+                )
+                .spacing(8)
+                .width(Length::Fill),
+            )
+            .id(SCROLLABLE_ID.clone()),
+        )
+        .align_y(Vertical::Bottom)
+        .height(Length::Fill)
+        .width(Length::Fill);
+
+        let composer = Container::new(
+            TextInput::new(
+                "Send a message...",
+                &self.compose_value,
+                ClientMessage::ComposerTyped,
+            )
+            .on_submit(ClientMessage::MessageSubmitted)
+            .padding(8),
+        )
+        .width(Length::Fill);
+
+        let content = Column::new().push(messages).push(composer).spacing(8);
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_y(Vertical::Bottom)
+            .padding(8)
+            .into()
+    }
 }
